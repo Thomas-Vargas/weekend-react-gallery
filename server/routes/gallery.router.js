ebc56bb908527acfab422bba252b03fc95ba02bb
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-03-24 09:07:31 -0500

    App now uses database instead of gallery.data.js.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
@@ -8,17 +9,33 @@ const galleryItems = require('../modules/gallery.data');
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const sqlText = `UPDATE "gallery" SET "likes" = "likes" + 1 WHERE "id" = $1;`
+
+    pool
+        .query(sqlText, [galleryId])
+        .then(result => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(error)
+            res.sendStatus(500)
+        })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const sqlText = `SELECT * FROM "gallery" ORDER BY "id";`;
+
+    pool
+        .query(sqlText)
+        .then(result  => {
+            res.send(result.rows)
+            console.log('success getting gallery')
+        })
+        .catch(error => {
+            console.log(error)
+            res.sendStatus(500)
+        })
 }); // END GET Route
 
 module.exports = router;
